67862b99d8753d3ded3e73b3c902f5ed09a8514c
Nayeem <125677005+nayeemashraf@users.noreply.github.com>
2023-12-01 18:30:22 +0530

    Update Scope.js

--- JavaScript/assets/Scope.js
+++ JavaScript/assets/Scope.js
@@ -1,16 +1,15 @@
 //💡 Scope determines the accessibility (visibility) of variables.
-// Space or environment in which a certain variable is declared.
+// It is the space or environment in which a certain variable is declared.
 
 // Scope of a variable: Region of our code where a certain variable can be accessed.
 
-// Before ES6(2015), JavaScript had only Global Scope and Function Scope. ES6 introduced two important new JavaScript keywords: let and const.
+// Before ES6(2015), JavaScript had only Global Scope and Function Scope. ES6 introduced two important JavaScript keywords: let and const.
 
-// variables declared with 'let' and 'const' have block scope in addition to global scope. That is, when they are declared inside a block, they can only be accessed within that block. when declared outside of a block, they can be accessed anywhere in the code.
+// Variables declared with 'let' and 'const' have block scope in addition to global scope. That is, when they are declared inside a block, they can only be accessed within that block. When declared outside of a block, they can be accessed anywhere in the code.
 
 // In JavaScript, by default we are in the global scope. In web browsers, it's the window scope (global object). Variables declared in the global scope become properties of the global object.
 
 //💡 Variables declared in global scope (outside of a code block {}) are known as global variables, Variables declared inside the code block are known as local variables.
-
 
 //💡 Block Scope
 // variable 'message' is only accessible within the code block.
@@ -20,7 +19,7 @@
 }
 console.log(message); //🔥 ReferenceError: message is not defined
 
-//* The code block can be a function, an if-else, a loop or an object.
+//* The code block can be a function, an if-else, or a loop.
 
 // We cannot access the 'message' variable outside of the 'start' function.
 function start() {
@@ -54,18 +53,18 @@ Gone();
 
 //💡 Block scoped variables are visible within the same block and any nested blocks.
 if (true) {
-  let person = "jhon smith";
+  let person = "john smith";
   {
-    let greet = "good bye";
-    console.log(greet, person); //🔥 'good bye jhon smith'
+    let farewell = "goodbye";
+    console.log(farewell, person); //🔥 'good bye john smith'
     // This inner block is able to access the 'person' variable because it is nested inside another block (in this case in 'if' block) where the variable ('person') is defined.
   }
-  console.log(person); //🔥 'jhon smith'
-  console.log(greet); //🔥 ReferenceError: greet is not defined
+  console.log(person); //🔥 'john smith'
+  console.log(farewell); //🔥 ReferenceError: farewell is not defined
 }
 
-//💡 It is valid to have variables with identical names in separate blocks. We are allowed to redeclare a variable outside of a code block. Since the scope of these variables is limited to the block in which they are defined hence they are separate variables independent of each other.
-// We declared variables with the same name. Here 'same' is defined both in global scope and local scope. When we call it inside the code block, it refers to the local variable. When we call it outside the code block in global scope, it refers to the global variable. Block-scoped variables are local in their respective code blocks with local scope.
+//💡 It is valid to have variables with identical names in separate blocks. Since the scope of these variables is limited to the block in which they are defined hence they are distinct variables.
+// We declared variables with the exact same name. Here 'same' is defined both in global scope and local scope. When we call it inside the code block, it refers to the local variable. When we call it outside the code block in global scope, it refers to the global variable. Block-scoped variables are local in their respective code blocks with local scope.
 
 const same = "I'm a global variable with global scope";
 
@@ -109,70 +108,87 @@ multiply(); //🔥 60
 
 //💡 A function defined inside another function (nested) can also access all variables defined in its parent function, and any other variables to which the parent function has access.
 const emoji = "👋";
-function Gone() {
-  let person = "jhon smith!"; // Function-scoped variable
+function Greeting() {
+  let person = "john smith!"; // Function-scoped variable
   if (true) {
-    let greet = "goodbye"; // Block-scoped variable
+    let greet = "Hello"; // Block-scoped variable
     console.log(greet, person, emoji);
     // Function-scoped variables are visible within any nested blocks, regardless of whether the code block is another function, an if-else, for-loop or any other block.
   }
 }
-Gone();
+Greeting();
 
-//* It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
-// A nested function example. Although we have already declared num1 and num2 in the global scope with values of 20 and 3, respectively, but when we call the add() function, it uses the variables declared within its parent function.
+// Variables which are declared within a function, as well as the function parameters, have local scope. That means they are only visible within that function.
+function myTest(p) {
+  const localVar = "foo";
+  if (true) {
+    const param = p;
+    console.log(param);
+  }
+  console.log(localVar);
+}
+
+myTest("Can you access me?");
+console.log(localVar, param); // Not accessible
+
+//💡 Shadowing
+// When a variable is declared in a certain scope having the same name defined on its outer scope and when we call the variable from the inner scope, the inner variable takes precedence within its scope, and any reference to that variable within that scope refers to the inner variable rather than the outer one.
+// OR
+// The variable whose scope is most immediate takes precedence.
+// OR
+// The variable in the inner scope (block scope) "shadows" the variable in the outer scope, meaning it takes precedence within its own scope.
+
+// Case 1.
+// In each case, 'y' that is just above the execution call takes precedence over any other 'y' variable before it.
+let y = 25;
+if (true) {
+  console.log("first code block", y);
+  if (true) {
+    let y = 40;
+    console.log("2nd code block", y);
+    if (true) {
+      y++;
+      console.log("innermost code block", y);
+    }
+    console.log(y);
+  }
+  console.log("first code block", y);
+}
+
+// Case 2.
+// Although we have already declared round1 and round2 in the global scope with values of 20 and 3, respectively, but when we call the add() function, it uses the variables declared within its parent function.
+const round1 = 20;
+const round2 = 3;
+const Player = "Kevin";
 function getScore() {
-  const num1 = 2;
-  const num2 = 3;
+  const round1 = 2;
+  const round2 = 3;
   function add() {
-    return `${Name} scored ${num1 + num2}`;
+    return `${Player} scored ${round1 + round2}`;
   }
   return add();
 }
 
-getScore(); //🔥 "Henry scored 5"
+getScore(); //🔥 "Kevin scored 5"
 
-// Variables which are declared within a function, as well as the function parameters, have local scope. That means they are only visible within that function.
-function myTest(para) {
-  const loc = "foo";
-  if (true) {
-    const funParameter = para;
-    console.log(funParameter);
-  }
-  console.log(loc);
-}
-
-myTest("Can you access me?");
-console.log(loc, funParameter); // Not accessible
-
-//💡 Precedence
-// Case 1.
-// The variable whose scope is most immediate takes precedence. In each case, 'myAge' variable that is just above the execution call takes precedence over any other 'myAge' variable before it.
-let lastName = "Bryan";
-let myAge = 25;
+// Case 3.
+// first console log statement is not able to access the 'x' variable defined in the global scope because the local variable 'x' shadows the global variable.
+const x = "Out";
 if (true) {
-  let myAge = 40;
-  let fName = "shaun";
-  console.log("inside Ist code block: ", fName, myAge);
-  if (true) {
-    console.log("inside 2nd code block: ", lastName, myAge);
-  }
-  console.log(fName, lastName, myAge);
+  console.log(x); //🔥 Cannot access 'x' before initialization
+  const x = "Shadow";
+  console.log(x);
 }
 
-// Case 2.
-// If there is a local variable with the same name within the block, it will shadow (take precedence over) any variable with the same name in an outer scope.
-const glo = 'some';
-if(true) {
-  console.log(glo); //🔥 Cannot access 'glo' before initialization // local variable takes precedence over global variable.
-  const glo = "shadow";
-  console.log(glo);
-}
-// first console log statement is not able to access the 'glo' variable defined in the global scope because the local variable 'glo' shadows the global variable.
+//💡 Global variables in JavaScript have a longer lifespan, persisting throughout the entire program execution.
+// Global variables in a web page context live until the page is discarded, like when you navigate to another page or close the window, or refresh the page.
+
+//💡 Local variables have short lives. They are created when the function is invoked, and deleted when the function is finished.
+// Once the function completes its execution, local variables go out of scope and are "deleted" or more precisely, they become inaccessible. The memory occupied by these local variables is typically freed up, and the variables cease to exist.
 
 //💡 All undeclared variables are global variables.
 // If we declare a variable without the 'let' or 'const' keyword, the variable is declared as a global variable. This means that the variable is accessible from anywhere in the code, including from other functions.
-//⛔ Declaring variables without keywords is a bad practice.
+//⛔ Declaring variables without keywords is a bad practice. So, ignore this weird behaviour.
 save = 60;
 if (true) {
   save = 40;
@@ -185,31 +201,27 @@ nameLess = "It's proven now that I'm actually a global variable"; // declared in
 console.log(nameLess);
 
 // Another Example with functions.
-// variableWithoutKeyword would not be accessible directly, it only becomes global after the function call.
+// withoutKeyword would not be accessible directly, it only becomes global after the function call.
 function myFunction() {
-  variableWithoutKeyword = "Global Variable";
+  withoutKeyword = "Global Variable";
 }
 
 myFunction();
-console.log(variableWithoutKeyword); // 'Global Variable'
+console.log(withoutKeyword); // 'Global Variable'
 
-variableWithoutKeyword = "Modified Value";
-console.log(variableWithoutKeyword); // 'Modified Value'
+withoutKeyword = "Modified Value";
+console.log(withoutKeyword); // 'Modified Value'
 
 // 'noLabel' becomes function-scoped. This means that the variable is accessible within the function and any nested functions, regardless of whether the parent function has been called or not.
 
 function parentFunction() {
   noLabel = "Function-scoped variable";
 
-  function nestedFunction() {
+  function childFunction() {
     console.log(noLabel); // Accessible within the nested function
   }
 
-  nestedFunction();
+  childFunction();
 }
 
 parentFunction();
-
-// Global variables live until the page is discarded, like when you navigate to another page or close the window.
-
-// Local variables have short lives. They are created when the function is invoked, and deleted when the function is finished.
